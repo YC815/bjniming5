0a575e230f81b14ed05b41b44fcf854e542c1b76
YC815 <minnecraft.YC815@gmail.com>
2023-12-16 15:21:06 +0800

    finish

--- pages/api/submit-data.js
+++ pages/api/submit-data.js
@@ -2,12 +2,6 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const { inputText } = req.body;
-
-      // 在这里调用你的Python脚本，处理 inputText 数据
-      // 例如，你可以使用 child_process 或者其他方法运行 Python 脚本
-      // ...
-
-      // 假设你的 Python 脚本返回一个结果
       const result = "Python script processed the data";
 
       res.status(200).json({ message: result });
--- py/main.py
+++ py/main.py
@@ -19,62 +19,59 @@ client = OpenAI(api_key=os.getenv('API_KEY'))
 
 def process_and_upload_to_instagram(user_text):
     global user, client
+    user = user_text
 
-    try:
-        user = user_text
+    # 使用 user 进行你的处理
+    ai_response = client.chat.completions.create(model="gpt-3.5-turbo", messages=[
+        {"role": "user", "content": "請在68字內簡單幽默的回答\"%s\"。" % (user)}
+    ])
+    ai_response = re.search(r"content='(.*?)', role", str(ai_response))
+    ai = str(ai_response.group(1))
+    print(ai)
+    user = user.replace('\n', ' ')
+    user = [user[i:i + 14] for i in range(0, len(user), 14)]
+    user = "\n".join(user)
+    ai = ai.replace('\n', ' ')
+    ai = [ai[i:i + 17] for i in range(0, len(ai), 17)]
+    ai = "\n".join(ai)
+    # 打開圖片、繪制文本、保存圖片等代码
+    image = Image.open('public/ins.jpg')
+    draw = ImageDraw.Draw(image)
 
-        # 使用 user 进行你的处理
-        ai_response = client.chat.completions.create(model="gpt-3.5-turbo", messages=[
-            {"role": "user", "content": "請在68字內簡單幽默的回答\"%s\"。" % (user)}
-        ])
-        ai_response = re.search(r"content='(.*?)', role", str(ai_response))
-        ai = str(ai_response.group(1))
-        print(ai)
-        user = user.replace('\n', ' ')
-        user = [user[i:i + 14] for i in range(0, len(user), 14)]
-        user = "\n".join(user)
+    # 加載一個支持中文的字體
+    font_path = "public/NotoSansTC-Regular.ttf"  # 替換成你的中文字體路徑
+    font = ImageFont.truetype(font_path, size=66)  # 設置字體大小
 
-        # 打開圖片、繪制文本、保存圖片等代码
-        image = Image.open('public/ins.jpg')
-        draw = ImageDraw.Draw(image)
+    # 繪制文本
+    text_position_user = (50, 66)  # 調整文字的位置
+    text_color = 'rgb(255, 255, 255)'  # 設定文字顏色
+    draw.text(text_position_user, user, fill=text_color, font=font)
 
-        # 加載一個支持中文的字體
-        font_path = "public/NotoSansTC-Regular.ttf"  # 替換成你的中文字體路徑
-        font = ImageFont.truetype(font_path, size=66)  # 設置字體大小
+    font = ImageFont.truetype(font_path, size=56)
+    text_position_ai = (50, 700)  # 調整文字的位置
+    draw.text(text_position_ai, ai, fill=text_color, font=font)
 
-        # 繪制文本
-        text_position_user = (50, 66)  # 調整文字的位置
-        text_color = 'rgb(255, 255, 255)'  # 設定文字顏色
-        draw.text(text_position_user, user, fill=text_color, font=font)
+    # 保存圖片
+    image.load()
+    background = Image.new("RGB", image.size, (255, 255, 255))
+    background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
 
-        font = ImageFont.truetype(font_path, size=56)
-        text_position_ai = (50, 700)  # 調整文字的位置
-        draw.text(text_position_ai, ai, fill=text_color, font=font)
+    background.save('public/ready.jpg', 'JPEG', quality=80)
 
-        # 保存圖片
-        image.load()
-        background = Image.new("RGB", image.size, (255, 255, 255))
-        background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
-
-        background.save('public/ready.jpg', 'JPEG', quality=80)
-
-        # 上传图片到 Instagram
-        cl = Client()
-        cl.login(os.getenv('USERNAME'), os.getenv('PASSWORD'))
-        media = cl.photo_upload(
-            "public/ready.jpg",
-            "濱江匿名網5.0",
-            extra_data={
-                "custom_accessibility_caption": "alt text example",
-                "like_and_view_counts_disabled": 0,
-                "disable_comments": 0,
-            }
-        )
-        media.dict()
-        print("上傳了")
-        return {'message': 'User text processed successfully'}
-    except Exception as e:
-        return {'error': str(e)}
+    # 上传图片到 Instagram
+    cl = Client()
+    cl.login(os.getenv('USERNAME'), os.getenv('PASSWORD'))
+    media = cl.photo_upload(
+        "public/ready.jpg",
+        "濱江匿名網5.0",
+        extra_data={
+            "custom_accessibility_caption": "alt text example",
+            "like_and_view_counts_disabled": 0,
+            "disable_comments": 0,
+        }
+    )
+    media.dict()
+    print("上傳了")
 
 
 @app.route('/process-user', methods=['POST', 'OPTIONS'])
@@ -89,12 +86,12 @@ def process_user_route():
                 'Access-Control-Allow-Headers': 'Content-Type',
             }
         )
+        print(response)
         return response
 
     user_text = request.json.get('user')
-    result = process_and_upload_to_instagram(user_text)
-    return jsonify(result)
+    process_and_upload_to_instagram(user_text)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="https://one-elf-proven.ngrok-free.app/process-user")
+    app.run(debug=True)
